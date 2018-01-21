f343aa25d4b9c6923221dce1584f5b138ded2de4
jacob ruleaux <jakeruleaux@gmail.com>
2018-01-21 15:14:34 -0800

    add text box and css to about file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './../styles/About.css';
 
 const About = () => {
   return(
-    <div>
+
+    <div className={styles.wrapper}>
+      <div className={styles.aboutdata}>
+        <h3>Title</h3>
+        <p>Text.........</p>
         <Link to='/'>Home</Link>
+      </div>
     </div>
   );
 };
